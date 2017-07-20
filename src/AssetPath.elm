57bd98f0236ae637a70491a917728e73845bc661
Brooke <brooke@noredink.com>
2017-07-20 14:51:56 -0700

    add src to AssetPath

--- src/AssetPath.elm
+++ src/AssetPath.elm
@@ -1,18 +1,22 @@
-module AssetPath exposing (Asset(..), url)
+module AssetPath exposing (Asset(..), src, url)
 
 {-| The core API of `AssetPath`: a union type and a function.
 
-@docs Asset, url
+@docs Asset, url, src
+
 -}
 
-import String
+import Html
+import Html.Attributes
 import Native.AssetPath
+import String
 
 
 {-| Tag a path to a static asset as such.
 
 Use `AssetUrl` to mark a fully qualified URL.
 `url` will return the tagged value as-is.
+
 -}
 type Asset
     = AssetPath String
@@ -35,6 +39,7 @@ In case such a meta tag is found, the return value is a concatenation
 of the content attribute of the tag and the `AssetPath` value.
 
 The meta tag needs to be present at the time Elm has been loaded.
+
 -}
 url : Asset -> String
 url asset =
@@ -44,6 +49,13 @@ url asset =
 
         AssetPath path ->
             toAbsoluteUrl path
+
+
+{-| Makes an Html src attribute from an asset.
+-}
+src : Asset -> Html.Attribute msg
+src asset =
+    Html.Attributes.src <| url asset
 
 
 toAbsoluteUrl : String -> String
